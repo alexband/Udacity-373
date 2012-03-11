bc0f9dfad88933957e6de5e7d734e9176f576995
huanghuang <huanghuang@douban.com>
2012-03-11 17:10:14 +0800

    sebastian's resampling wheel

--- 3-19.py
+++ 3-19.py
@@ -147,6 +147,19 @@ for i in xrange(N):
     p3.append(p[bisect.bisect(acc, random.random()*s)])
 
 #weighted sample solution 2
+index = int(random.random() * N)
+beta = 0.0
+mw = max(w)
+for i in range(N):
+    beta += random.random() * 2.0 * mw
+    while beta > w[index]:
+        beta -= w[index]
+        index = (index + 1) % N
+    p3.append(p[index])
+p = p3
+
+
+
 
 
 
